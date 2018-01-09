9d6267991f6655157ccbba2d89d931a3331a733c
Marwan Alani <marwan@alani.ca>
2018-01-09 17:46:26 -0500

    Initial Commit

---
+++ NASA Pic of the Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NASA Pic of the Day
+//
+//  Created by Marwan Al-ani on 1/9/18.
+//  Copyright © 2018 Marwan Al-ani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ NASA Pic of the DayTests/NASA_Pic_of_the_DayTests.swift
@@ -0,0 +1,36 @@
+//
+//  NASA_Pic_of_the_DayTests.swift
+//  NASA Pic of the DayTests
+//
+//  Created by Marwan Al-ani on 1/9/18.
+//  Copyright © 2018 Marwan Al-ani. All rights reserved.
+//
+
+import XCTest
+@testable import NASA_Pic_of_the_Day
+
+class NASA_Pic_of_the_DayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
